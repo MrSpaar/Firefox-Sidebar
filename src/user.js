c482f0ea5c5e5674924add9c244ec971e7666b9a
MrSpaar <julien.pistre@icloud.com>
2025-03-09 19:16:39 +0100

    feature: homepage same as newtab page

--- src/user.js
+++ src/user.js
@@ -10,3 +10,6 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 // Disable search engine and top sites suggestions
 user_pref("browser.urlbar.suggest.engines", false);
 user_pref("browser.urlbar.suggest.topsites", false);
+
+// Set homepage to newtab page
+user_pref("browser.startup.homepage", "about:newtab");
